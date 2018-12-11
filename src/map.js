25fbebf32081f56322e44af2d145c05025c25724
Ivan Malagon <hacheka@gmail.com>
2018-12-11 17:30:38 +0100

    05 - Add CARTO VL

--- src/map.js
+++ src/map.js
@@ -1,9 +1,16 @@
+let viz;
+let account = {
+  username: 'cartovl',
+  apiKey: 'default_public',
+  dataset: 'ny_listings'
+};
+
 function initMapboxGL () {
   map = new mapboxgl.Map({
     container: 'map',
     style: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',
-    center: [-3.7038, 40.4168],
-    zoom: 11,
+    center: [-74, 40.7128],
+    zoom: 10,
     scrollZoom: false,
     dragRotate: false,
     touchZoomRotate: false
@@ -12,4 +19,22 @@ function initMapboxGL () {
     showCompass: false
   });
   map.addControl(nav, 'bottom-left');
+}
+
+function loadMap (categoriesCb) {
+  const { username, apiKey, dataset } = account;
+  carto.setDefaultAuth({
+    user: username,
+    apiKey: apiKey
+  });
+
+  const source = new carto.source.SQL( `SELECT * from ${dataset} WHERE price < 500`);
+  viz = new carto.Viz(`
+    width: 8,
+    color: opacity(rgb(0, 0, 255), 0.35),
+    strokeWidth: 0
+  `);
+
+  layer = new carto.Layer(dataset, source, viz);
+  layer.addTo(map);
 }
