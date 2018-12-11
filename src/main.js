1e81ab1be82a3b236cd2c7c0aecd808edb03b843
Ivan Malagon <hacheka@gmail.com>
2018-12-11 17:37:27 +0100

    07 - Add category widget

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 function onLoad () {
   initMapboxGL();
   loadMap();
+  feedWidget();
+}
+
+function feedWidget () {
+  const categoryWidget = document.getElementById('neighbourhoods');
+  categoryWidget.categories = [
+    { name: 'Bars & Restaurants', value: 1000, color: '#FABADA' },
+    { name: 'Fashion', value: 900 },
+    { name: 'Grocery', value: 800 },
+    { name: 'Health', value: 400 },
+    { name: 'Shopping mall', value: 250 },
+    { name: 'Transportation', value: 1000 },
+    { name: 'Leisure', value: 760 }
+  ];
 }
 
 window.onload = onLoad;
